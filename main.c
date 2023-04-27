ed38abe13e12fe4869f014fff1ad2eb091d11564
Nguyen Trong Minh <bammbooman@gmail.com>
2023-04-27 20:33:57 +0700

    add main.c file

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct studentInfo{
+	char fullName[20];
+	char ID[10];
+	char birthDate[11];
+	float algebra;
+	float calculus;
+	float basicProgramming;
+	float GPA;
+};
+
+int main(){
+	return 0;
+}
