cd2ad8ad9fba3978db43192038bb5ea2a901dc34
hheulwen <pnmchau92@gmail.com>
2023-04-28 15:55:46 +0700

    add task2

--- main.c
+++ main.c
@@ -15,14 +15,46 @@ struct studentInfo{
 
 int main(){
 	// Task1 - Enter student number
-    int studentNum;
+    int number;
     printf("Enter the number of students: ");
-    scanf("%d", &studentNum);
+    scanf("%d", &number);
 
-    while(studentNum > 1000) {
+    while(number > 1000) {
         printf("Your number is too high. Maximum number of student is 1000. Please enter again: ");
-        scanf("%d", &studentNum);
+        scanf("%d", &number);
     }
 
+	//Task2 - get student info
+	struct studentInfo students[number];
+    
+    for (int i = 0; i < number; i++) {
+        printf("Enter student %d name: ", i+1);
+        fgets(students[i].fullName, sizeof(students[i].fullName), stdin); //use fgets instead of scanf
+        students[i].fullName[strcspn(students[i].fullName, "\n")] = 0; // Remove trailing newline
+ 
+        printf("Enter student %d ID: ", i+1);
+        scanf("%s", students[i].ID);
+        getchar(); // consume the newline character left in the input stream
+
+        printf("Enter student %d birthdate: ", i+1);
+        scanf("%s", students[i].birthDate);
+        getchar();
+
+        printf("Enter student %d Linear Algebra point: ", i+1);
+        scanf("%f", &students[i].algebra);
+        getchar();
+
+        printf("Enter student %d Calculus point: ", i+1);
+        scanf("%f", &students[i].calculus);
+        getchar();
+        
+        printf("Enter student %d Basic Programming point: ", i+1);
+        scanf("%f", &students[i].basicProgramming);
+        getchar(); // consume the newline character left in the input stream
+        
+        printf("Enter student %d GPA: ", i+1);
+        scanf("%f", &students[i].GPA);
+        getchar(); // consume the newline character left in the input stream
+    }
 	return 0;
 }
