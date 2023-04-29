072637a8af5ca5d93271e1f7ce2466d00866da89
hheulwen <pnmchau92@gmail.com>
2023-04-29 15:53:49 +0700

    add task 5

--- main.c
+++ main.c
@@ -12,19 +12,47 @@ struct studentInfo{
 	float basicProgramming;
 	float GPA;
 };
-
+// Task 5 - Highest, lowest GPA student and highest basic programming point 
+struct studentInfo highest_gpa(struct studentInfo students[], int number) {
+    struct studentInfo highest = students[0];
+    for (int i = 1; i < number; i++) {
+        if (students[i].GPA > highest.GPA) {
+            highest = students[i];
+        }
+    }
+    return highest;
+}
+struct studentInfo lowest_gpa(struct studentInfo students[], int number) {
+    struct studentInfo lowest = students[0];
+    for (int i = 1; i < number; i++) {
+        if (students[i].GPA < lowest.GPA) {
+            lowest = students[i];
+        }
+    }
+    return lowest;
+}
+struct studentInfo bp_Point(struct studentInfo students[], int number) {
+    struct studentInfo highest_bp_Point = students[0];
+    for (int i = 1; i < number; i++) {
+        if (students[i].basicProgramming > highest_bp_Point.basicProgramming) {
+            highest_bp_Point = students[i];
+        }
+    }
+    return highest_bp_Point;
+}
 int main(){
 	// Task1 - Enter student number
     int number;
     printf("Enter the number of students: ");
     scanf("%d", &number);
+    getchar();
 
     while(number > 1000) {
         printf("Your number is too high. Maximum number of student is 1000. Please enter again: ");
         scanf("%d", &number);
     }
 
-	//Task2 - get student info
+	//Task2 - Get student info
 	struct studentInfo students[number];
     
     for (int i = 0; i < number; i++) {
@@ -56,5 +84,12 @@ int main(){
         scanf("%f", &students[i].GPA);
         getchar(); // consume the newline character left in the input stream
     }
+    // Task 5
+    struct studentInfo highest_gpa_student = highest_gpa(students, number);
+    printf("Student with highest GPA:\n %s\t%s\t%2.1f", highest_gpa_student.fullName, highest_gpa_student.ID, highest_gpa_student.GPA);
+    struct studentInfo lowest_gpa_student = lowest_gpa(students, number);
+    printf("\nStudent with lowest GPA:\n %s\t%s\t%2.1f", lowest_gpa_student.fullName, lowest_gpa_student.ID, lowest_gpa_student.GPA);
+    struct studentInfo highest_bp_student = bp_Point(students, number);
+    printf("\nStudent with highest BP mark:\n %s\t%s\t%2.1f", highest_bp_student.fullName, highest_bp_student.ID, highest_bp_student.basicProgramming);
 	return 0;
 }
